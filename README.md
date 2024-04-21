9a00d24384b705d8702dcb9dfb1ebb83fe5d9a43
Deborah McVey <deborah.mcvey@gmail.com>
2024-04-21 10:32:03 -0400

    added navbar, completed first video

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-"Angular 17 Login | With API Integration" by Learning Partner
+Video 1. "Angular 17 Login | With API Integration" by Learning Partner
 https://www.youtube.com/watch?v=Dvqe0uIhBxQ
 
 "Animated Login Form using HTML CSS Only"
@@ -7,4 +7,10 @@ https://codepen.io/ajay-dhangar/pen/YzgPBWd
 https://apipheny.io/free-api/
 https://randomuser.me/api/
 
+Bootstrap 5 Colored Navbar for layout: https://www.w3schools.com/bootstrap5/bootstrap_navbar.php
 
+Video 2. "Angular 17 Login with JWT token | With API | Angular Interceptor" by Learning Partner
+https://www.youtube.com/watch?v=mWERmpulRIw
+
+Video 3. "Guard In Angular 17 | Angular Authentication & Authorization | Angular 17" by Learing Partner
+https://www.youtube.com/watch?v=6_tRwY52Llg
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,25 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HttpClientModule], //do without modules
+  imports: [
+    FormsModule,
+    HttpClientModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginObj: Login;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {
     this.loginObj = new Login();
   }
 // do a login service
@@ -20,7 +27,8 @@ export class LoginComponent {
     debugger;
     this.http.post('https://randomuser.me/api/', this.loginObj).subscribe((res:any) => {
       if(res.result) {
-        alert('Login Success')
+        alert('Login Success');
+        this.router.navigateByUrl('./dashboard')
       } else {
         alert(res.message)
       }
